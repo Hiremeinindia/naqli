f724e198433721c64132320bec88f85dc0c7fdd2
Priya <prakiya318@gmail.com>
2024-02-24 11:05:18 +0530

    Homepage responsive

---
+++ lib/bookingdialog.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application_1/dashboard.dart';
+import 'package:flutter_application_1/homepage.dart';
+import 'package:flutter_application_1/loginPage.dart';
+
+class BookingDialog extends StatefulWidget {
+  const BookingDialog();
+
+  @override
+  _BookingDialogState createState() => _BookingDialogState();
+}
+
+class _BookingDialogState extends State<BookingDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
