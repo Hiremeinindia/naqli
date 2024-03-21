836db947b83624306ad7606a97413d92c8e94d6f
Priya <prakiya318@gmail.com>
2024-03-21 16:45:37 +0530

    data fetched and displayed from firestore in registration

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _MyAppState extends State<MyApp> {
       supportedLocales: AppLocalizations.supportedLocales,
       locale: _locale,
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: CreateAccount(),
     );
   }
 }
