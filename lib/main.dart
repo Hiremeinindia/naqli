5660d97de421f535d1ab5b08173fc7307011c3ea
Priya <prakiya318@gmail.com>
2024-03-25 12:36:44 +0530

    otp verified successfully and payment page edited

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,7 @@ class _MyAppState extends State<MyApp> {
       supportedLocales: AppLocalizations.supportedLocales,
       locale: _locale,
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: SingleUserDashboardPage(),
     );
   }
 }
