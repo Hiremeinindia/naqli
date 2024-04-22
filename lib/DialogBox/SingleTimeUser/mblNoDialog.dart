aa7076ec27c6121b4a47a8d4767b5fad55c4bb2b
Naqli2024 <naqli2024@gmail.com>
2024-04-22 16:11:32 +0530

    homepage responsive

--- lib/DialogBox/SingleTimeUser/mblNoDialog.dart
+++ lib/DialogBox/SingleTimeUser/mblNoDialog.dart
@@ -531,22 +531,16 @@ class _MblNoDialogState extends State<MblNoDialog> {
                                                             MainAxisAlignment
                                                                 .center,
                                                         children: [
-                                                          Padding(
-                                                            padding:
-                                                                const EdgeInsets
-                                                                    .only(
-                                                                    right: 50),
-                                                            child: ImageIcon(
-                                                              NetworkImage(
-                                                                  'https://firebasestorage.googleapis.com/v0/b/naqli-5825c.appspot.com/o/approved.png?alt=media&token=1464e391-a8a7-4e1f-9ff8-90154603b7c9'),
-                                                              color: Color
-                                                                  .fromRGBO(
-                                                                      60,
-                                                                      55,
-                                                                      148,
-                                                                      1),
-                                                              size: 30,
-                                                            ),
+                                                          ImageIcon(
+                                                            NetworkImage(
+                                                                'https://firebasestorage.googleapis.com/v0/b/naqli-5825c.appspot.com/o/approved.png?alt=media&token=1464e391-a8a7-4e1f-9ff8-90154603b7c9'),
+                                                            color:
+                                                                Color.fromRGBO(
+                                                                    60,
+                                                                    55,
+                                                                    148,
+                                                                    1),
+                                                            size: 30,
                                                           ),
                                                           SizedBox(width: 5),
                                                           Text(
